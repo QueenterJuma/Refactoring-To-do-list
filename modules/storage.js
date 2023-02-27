fba5b7b345e61bd7fb3837acf213c1b365082c15
QueenterJuma <jumahqueenter@gmail.com>
2023-02-28 00:35:49 +0300

    Fixed linter

--- modules/storage.js
+++ modules/storage.js
@@ -1,11 +1,10 @@
-const saveTasks = (tasks) =>
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+const saveTasks = (tasks) => localStorage.setItem('tasks', JSON.stringify(tasks));
 // const saveTasks = (tasks) => {
 //   localStorage.setItem("tasks", JSON.stringify(tasks));
 // };
 
 const loadTasks = () => {
-  const tasksJson = localStorage.getItem("tasks");
+  const tasksJson = localStorage.getItem('tasks');
   if (tasksJson) {
     return JSON.parse(tasksJson);
   }
